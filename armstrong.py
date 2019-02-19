cbabaf8e803fc4fd35c10bbbab2b5c053badac0a
MonishaRamamoorthy <46843348+MonishaRamamoorthy@users.noreply.github.com>
2019-02-19 16:03:33 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,17 @@
+#monisha
+lower = int(input())
+upper = int(input())
+ 
+for num in range(lower,upper + 1):
+   # initialize sum
+   sum = 0
+ 
+   # find the sum of the cube of each digit
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+ 
+   if num == sum:
+       print(num)
