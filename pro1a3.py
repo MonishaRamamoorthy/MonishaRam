23cdb6354876d14911c95872c6828d1582694a5e
MonishaRamamoorthy <46843348+MonishaRamamoorthy@users.noreply.github.com>
2019-02-25 21:51:13 +0530

    Rename pro1a3,py to pro1a3.py

---
+++ pro1a3.py
@@ -0,0 +1,14 @@
+#monisha
+a,b =map(str,input().split())
+c = 0
+c+=abs(len(b)-len(a))
+if(len(a)<=len(b)):
+	max = b
+	min = a
+else:
+	max = a
+	min = b
+for i in range(len(min)):
+	if(min[i]!=max[i]):
+		c+=1
+print(c)
