fb87773ff9937dc736ad32a7d64e27cc43cbc95d
MonishaRamamoorthy <46843348+MonishaRamamoorthy@users.noreply.github.com>
2019-06-03 16:01:04 +0530

    Create pro1a19.py

---
+++ pro1a19.py
@@ -0,0 +1,14 @@
+#monisha
+n=int(input())
+i=0
+globallist=[]
+while(i<n):
+    lis=list(map(int,input().split(" ")))
+    globallist.extend(lis)
+    i=i+1
+
+globallist.sort() 
+i=0
+while(i<len(globallist)):
+    print(globallist[i],end=" ")
+    i+=1
