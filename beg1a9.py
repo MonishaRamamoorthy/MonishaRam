081ad88eb48d307c27dfae52bfd4e3ee543e511b
MonishaRamamoorthy <46843348+MonishaRamamoorthy@users.noreply.github.com>
2019-06-05 16:51:07 +0530

    Create beg1a9.py

---
+++ beg1a9.py
@@ -0,0 +1,15 @@
+#monisha
+N,K=input().split()
+N=int(N)
+K=int(K)
+s=0
+if(N>0):
+    if(K>0 and K<=N):
+        for i in range(1,K+1):
+            s=s+i
+        print(s)
+    else:
+        print("invalid")
+else:
+    print("invalid")
+
