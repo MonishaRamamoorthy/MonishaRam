86125eff9b50ae08decd04d2c5d469e308827fea
MonishaRamamoorthy <46843348+MonishaRamamoorthy@users.noreply.github.com>
2019-06-06 15:10:33 +0530

    Create hun1a7.py

---
+++ hun1a7.py
@@ -0,0 +1,14 @@
+#monisha
+n=int(input())
+a = [int(y) for y in input().split()]
+for i in range(0,n):
+    if i<n-1:
+        b=' '
+    else:
+        b=''
+    if i%2==0:
+        if a[i]%2!=0:
+            print(a[i],end=b)
+    elif i%2!=0:
+        if a[i]%2==0:
+            print(a[i],end=b)
