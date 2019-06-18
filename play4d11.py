6d326e4036a10454a05d966a4a3b9f347acb7b94
MonishaRamamoorthy <46843348+MonishaRamamoorthy@users.noreply.github.com>
2019-06-18 18:24:06 +0530

    Create play4d11.py

---
+++ play4d11.py
@@ -0,0 +1,10 @@
+#monisha
+s=input()
+li=list(s)
+if(li[0]=='(' and li[-1]==')'):
+    if(li.count('(')==li.count(')')):
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
